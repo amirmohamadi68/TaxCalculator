b7ae75699d24e04cd6c54fda1581e7bed309c320
Amir Mohamadi <a.mohamadi.d68@gmail.com>
2024-07-05 22:49:16 +0330

    Create Domain Layer

---
+++ TaxCalculator.Domain/Entities/TaxExemptVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxCalculator.Domain.Common;
+
+namespace TaxCalculator.Domain.Entities
+{
+    public record TaxExemptVehicle : EntityBase
+    {
+        public long CityTaxRuleId { get; set; }
+
+        public CityTaxRule CityTaxRule { get; set; } = null!;
+
+        public string VehicleType { get; set; } = null!;
+    }
+}
